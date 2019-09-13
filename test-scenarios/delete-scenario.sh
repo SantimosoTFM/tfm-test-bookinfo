a831ae5699646a23d429ae3bc94b58a951cba9f3
Santiago Mora Soler <Santiago.Mora.Soler1@ibm.com>
2019-09-13 11:39:10 +0200

    latests version

---
+++ test-scenarios/delete-scenario.sh
@@ -0,0 +1,3 @@
+
+kubectl delete -f bookinfo-dev/k8s -n bookinfo-dev
+kubectl delete -f bookinfo-pro/k8s -n bookinfo-pro
---
+++ test-scenarios/start-scenario.sh
@@ -0,0 +1,4 @@
+
+kubectl apply -f bookinfo-dev/k8s -n bookinfo-dev
+kubectl apply -f bookinfo-pro/k8s -n bookinfo-pro
+
