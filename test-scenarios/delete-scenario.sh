48e6664762f36aee2ec9e6a264f696135fa0d469
Santiago Mora Soler <Santiago.Mora.Soler1@ibm.com>
2019-10-16 11:50:56 +0200

    added lab test

--- test-scenarios/delete-scenario.sh
+++ test-scenarios/delete-scenario.sh
@@ -1,3 +1,4 @@
 
+kubectl delete -f bookinfo-lab/k8s -n bookinfo-lab
 kubectl delete -f bookinfo-dev/k8s -n bookinfo-dev
 kubectl delete -f bookinfo-pro/k8s -n bookinfo-pro
--- test-scenarios/start-scenario.sh
+++ test-scenarios/start-scenario.sh
@@ -1,4 +1,6 @@
 
+kubectl apply -f bookinfo-lab/k8s -n bookinfo-lab
+kubectl apply -f bookinfo-lab/k8s-rbac -n bookinfo-lab
 kubectl apply -f bookinfo-dev/k8s -n bookinfo-dev
 kubectl apply -f bookinfo-pro/k8s -n bookinfo-pro
 
